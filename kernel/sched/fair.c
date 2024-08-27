ce84180bd379c07777e4595104a0b0dfba333c86
danya2271 <dany2271@yandex.ru>
2024-08-27 21:49:25 +0500

    kernel: sched: fixes for compilation

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3463,8 +3463,7 @@ static long calc_group_runnable(struct cfs_rq *cfs_rq, long shares)
 	load_avg = max(cfs_rq->avg.load_avg,
 				   scale_load_down(cfs_rq->load.weight));
 
-	runnable = max(cfs_rq->avg.runnable_load_avg,
-				   scale_load_down(cfs_rq->runnable_weight));
+	runnable = READ_ONCE(cfs_rq->avg.runnable_avg);
 
 	runnable *= shares;
 	if (load_avg)
